175b5e53c4db7952314a702847300af0bbc6c229
Trevor Sullivan <Trevortds@users.noreply.github.com>
2023-05-25 11:29:29 -0500

    configure langchain

--- node-app/src/controllers/ai.controller.ts
+++ node-app/src/controllers/ai.controller.ts
@@ -4,10 +4,10 @@ import aiService from "../services/ai.service"
 class AiController {
 
 
-    processRequest(req: Request, res: Response){
-        const requestData = req.body
-        const id = aiService.process(requestData)
-        res.status(201).send({ "id": id })
+    async processRequest(req: Request, res: Response){
+        const requestData = req.body.prompt
+        const result = await aiService.process(requestData)
+        res.status(201).send({ "result": result })
     }
 }
 
